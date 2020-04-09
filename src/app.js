31c59cccba350ac9b6698c5fa98f9e321568d557
Victor Rocha <victor.barboza.15@gmail.com>
2020-04-08 21:31:02 -0400

    almost done

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,67 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+    return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs, likes } = request.body
+  const id = uuid()
+
+  repositories.push({
+    id,
+    title,
+    url,
+    techs,
+    likes
+  })
+
+  return response.json(repositories)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+  const { title, url, techs } = request.body
+
+  const indexOfRepo = repositories.findIndex(repo => repo.id === id)
+
+  if( indexOfRepo < 0)
+    return response.status(400)
+
+  const oldLikes = repositories[indexOfRepo].likes
+
+  const newRepo = {
+    id,
+    title,
+    url,
+    techs,
+    likes: oldLikes
+  }
+  repositories[indexOfRepo] = newRepo
+  return response.json(repositories)
 });
 
-app.delete("/repositories/:id", (req, res) => {
-  // TODO
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params
+
+  const indexOfRepo = repositories.findIndex(repo => repo.id === id)
+
+  if( indexOfRepo < 0)
+    return response.status(400)
+  
+  repositories.splice(indexOfRepo, 1)
+  return response.status(204).send()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params
+  const indexOfRepo = repositories.findIndex(repo => repo.id === id)
+
+  if( indexOfRepo < 0)
+    return response.status(400)
+
+  repositories[indexOfRepo].likes += 1
+  return response.json(repositories[indexOfRepo])
 });
 
 module.exports = app;
